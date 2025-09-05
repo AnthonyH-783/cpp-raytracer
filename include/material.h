49c7ea93f34a785cacc628be72d61eea7e0f8d43
Anthony <anthonnhani@gmail.com>
2025-09-05 16:48:26 -0600

    [Feature + Test] Included the Schlick Approximation to account for varying reflectance by angle

--- include/material.h
+++ include/material.h
@@ -66,9 +66,19 @@ class dielectric : public material {
         double ri = rec.front_face ? (1.0/refraction_index) : refraction_index; // Refractive index ratio
         // Calculate refraction using Snell's Law
         vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, rec.normal, ri);
-        // Create scattered ray with refracted direction
-        scattered = ray(rec.p, refracted);
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+        // Check for total internal reflection
+        bool cannot_refract = ri * sin_theta > 1.0;
+        vec3 direction;
+
+         if (cannot_refract || reflectance(cos_theta, ri) > random_double()) // Reflect the ray
+         
+            direction = reflect(unit_direction, rec.normal);
+        else // Otherwise, refract the ray
+            direction = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, direction); // Create scattered ray
         return true;
     }
 
@@ -76,6 +86,14 @@ class dielectric : public material {
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media
     double refraction_index;
+
+    // Schlick's approximation for reflectance
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
 };
 
 #endif
