2bd6702e185f208bf13328e584b290d2272a6f80
Anthony <anthonnhani@gmail.com>
2025-09-05 16:25:12 -0600

    [Feature] Added a class for dielectrics (clear materials that reflect AND refract). For now, it only refracts

--- include/material.h
+++ include/material.h
@@ -55,4 +55,27 @@ class metal : public material {
     double fuzz; // Fuzziness factor (0 = perfect mirror, 1 = very fuzzy)
 };
 
+class dielectric : public material {
+  public:
+  // Constructor to initialize dielectric material with refractive index
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        attenuation = color(1.0, 1.0, 1.0); // No attenuation for dielectric materials
+        double ri = rec.front_face ? (1.0/refraction_index) : refraction_index; // Refractive index ratio
+        // Calculate refraction using Snell's Law
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, ri);
+        // Create scattered ray with refracted direction
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+  private:
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    double refraction_index;
+};
+
 #endif
